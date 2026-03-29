ba32fb323d734a12b4f28140f49c27b860020807
thkx <oo932758@gmail.com>
2026-03-29 23:13:41 +0800

    AI 学习

--- cmd/ai/main.go
+++ cmd/ai/main.go
@@ -10,24 +10,30 @@ import (
 )
 
 func main() {
-	ctx := context.Background()
-
-	file, err := os.Open("./test.pdf")
+	// 打开 PDF
+	file, err := os.Open("test.pdf")
 	if err != nil {
-		log.Panicln(err)
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// 获取文件大小
+	fi, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	size := fi.Size()
+
+	// 创建 PDF loader
+	loader := documentloaders.NewPDF(file, size)
+
+	// 加载
+	docs, err := loader.Load(context.Background())
+	if err != nil {
+		log.Fatal("PDF 加载失败:", err)
 	}
 
-	docs := documentloaders.NewPDF(file, 100)
-	documents, err := docs.Load(ctx)
-	if err != nil {
-		log.Panicln(err)
-	}
-	fmt.Println(documents)
-	// AgentNode()
-
-	// stringPromptTemplates()
-
-	// OllamaPrompt()
-
-	// OllamaChat()
+	fmt.Println("✅ PDF 加载成功！")
+	fmt.Println("页数:", len(docs))
+	fmt.Println("内容:", docs[0].PageContent)
 }
